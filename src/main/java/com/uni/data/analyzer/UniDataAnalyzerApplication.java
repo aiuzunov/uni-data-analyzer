b1b27fe52b61d5c43ea432a39c5976fe45cb34b4
ivanmilevtues <ivanmilevtues@gmail.com>
2021-04-19 12:06:14 +0300

    Initial commit

---
+++ src/main/java/com/uni/data/analyzer/UniDataAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.uni.data.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniDataAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniDataAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uni/data/analyzer/UniDataAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.data.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniDataAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
